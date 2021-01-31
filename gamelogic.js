1f6e2363bcc4b74dcabe4f36736166a484c43a3e
LucasGoe <75021271+LucasGoe@users.noreply.github.com>
2021-01-31 11:44:19 +0100

    bonus doelen gemaakt

--- gamelogic.js
+++ gamelogic.js
@@ -68,16 +68,6 @@ function isGameWon(word, guesses) {
     return true;
 }
 
-    // EERSTE idee TODO-OPZET isGameWon
-    // - [ ] isGameWon is false zolang alle letters nog niet zijn geraden
-    // - [ ] maak een loop die geraden letters checkt
-    // let gameWon = false;
-    // for (let index = 0; index < lettersInWordArray.length; index++) {
-    //   if ();
-    // - [ ] zitten ze in het te raden woord Array
-    // - [ ] zoja, verklein de Array
-    // - [ ] is geraden letter laatste element in Array, return isGameWon is true
-
 function isGameLost(word, guesses) {
     // DOEL:
     // word = "javascript" guesses = ["q", "w", "e", "y", "u", "o", "d"]; -> true
@@ -100,7 +90,7 @@ function isGameLost(word, guesses) {
         // - [x] Als false "+ doen" -> counter met 1 verhogen
         if (!isLetterGuessed) {
             count++;
-            console.log(count);
+            console.log("Aantal fout: ", count);
         }
     }
     if (count === 1) {
@@ -109,7 +99,7 @@ function isGameLost(word, guesses) {
             "|\n" +
             "|\n" +
             "|\n" +
-            "===========")
+            "===========");
     }
     if (count === 2) {
         console.log("__________\n" +
@@ -118,7 +108,7 @@ function isGameLost(word, guesses) {
             "|\n" +
             "|\n" +
             "|\n" +
-            "===========")
+            "===========");
     }
     if (count === 3) {
         console.log("__________\n" +
@@ -127,7 +117,7 @@ function isGameLost(word, guesses) {
             "|\n" +
             "|\n" +
             "|\n" +
-            "===========")
+            "===========");
     }
     if (count === 4) {
         console.log("__________\n" +
@@ -136,7 +126,7 @@ function isGameLost(word, guesses) {
             "|\n" +
             "|\n" +
             "|\n" +
-            "===========")
+            "===========");
     }
     if (count === 5) {
         console.log("__________\n" +
@@ -154,7 +144,7 @@ function isGameLost(word, guesses) {
             "|       O\n" +
             "|\n" +
             "|\n" +
-            "===========")
+            "===========");
     }
     if (count === 7) {
         console.log("__________\n" +
@@ -163,17 +153,17 @@ function isGameLost(word, guesses) {
             "|       O\n" +
             "|      / \\\n" +
             "|\n" +
-            "===========")
+            "===========");
     }
     //console.log('wat is dit?', isLetterGuessed)
-    // - [ ] aan het eind van de functie if statement
-    // - [ ] if
-    //- [ ] count is 7 -> return true
+    // - [x] aan het eind van de functie if statement
+    // - [x] if
+    // - [x] count is 7 -> return true
     if (count === 7) {
         return true;
     }
-    // - [ ] count is minder dan 7 -> return false
-    if (count <= 7) {
+    // - [x] count is minder dan 7 -> return false
+    if (count < 7) {
         return false;
     }
 }
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const { question } = require("readline-sync");
 const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
 
-function game(word, guesses) {
+function game(word,guesses) {
   console.log("Dit heb je tot nu toe geraden: ", guesses);
   const wordSoFar = displayWordSoFar(word, guesses);   // toegevoegd om wordSoFar
   console.log(wordSoFar);                              // te laten zien
-  const letter = question("Raad een letter: ");
+  const letter = question("Raad een letter: ").substr(0, 1).toLowerCase();
   // voeg de geraden letter toe aan de array met guesses
   guesses.push(letter);
 
   const playerWon = isGameWon(word, guesses);
   if (playerWon) {
-    console.log("Je hebt gewonnen!");
+    console.log("Gefeliciteerd! Je hebt gewonnen.\n");
+    console.log("Volgende ronde!\n");
     return;
   }
   const playerLost = isGameLost(word, guesses);
   if (playerLost) {
-    console.log("Helaas, je hebt verloren!");
+    console.log("Helaas! Je hebt verloren.\n");
+    console.log("Volgende ronde!\n");
     return;
   }
   // volgende ronde! we roepen game nog een keer aan
